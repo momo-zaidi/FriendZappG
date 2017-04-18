2b8f14423e7a35b3be1e13050171f23e6d4c08bf
mickado94 <mickaellievens@hotmail.com>
2017-04-18 15:41:13 +0200

    Update GetInscription.php

--- GetInscription.php
+++ GetInscription.php
@@ -1,1 +1,9 @@
+<?php
+$prenom=$_POST[prenom];
+$nom=$_POST[nom];
+$email=$_POST[maill];
+$password=$_POST[pass];
+$naissance=$_POST[datenaiss];
+print("$prenom $nom $email $password $naissance");
+?>
 
