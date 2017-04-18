d69dc5a202ae44cd8e4e6349be1d5f5e475a9254
abdelrahman12 <abdel-x6@hotmail.com>
2017-04-18 15:42:03 +0200

    Update

--- GetInscription.php
+++ GetInscription.php
@@ -1,9 +1,13 @@
 <?php
-$prenom=$_POST[prenom];
-$nom=$_POST[nom];
-$email=$_POST[maill];
-$password=$_POST[pass];
-$naissance=$_POST[datenaiss];
-print("$prenom $nom $email $password $naissance");
-?>
-
+				
+				if (isset($_POST['inscrire']))
+				{
+				
+				IF    (empty($_POST['nom']) or empty($_POST['prenom']) or empty($_POST['password']) or empty($_POST['mail']) or empty($_POST['age']))
+				{
+					  
+						
+					print "<div class='alert alert-danger'>
+                          <strong>Remplissez les champs !</strong>
+                          </div>";
+				}
