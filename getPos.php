8e33f8cab5b8f0d6fab83ca60006b74092722845
VladimirTulcinsky <vladimir.tul@gmail.com>
2017-04-24 14:52:12 +0200

    Add files via upload

---
+++ getPos.php
@@ -0,0 +1,29 @@
+<?php
+function getPos() {
+			navigator.geolocation.getCurrentPosition(onSuccess, onError, { timeout: 30000 });
+			geocoder = new google.maps.Geocoder();
+
+			function onSuccess(position) {
+
+				$geolocation = 
+                {
+					lat: position.coords.latitude,
+					lng: position.coords.longitude
+				};
+
+				geolocation = JSON.stringify(geolocation);
+				
+                echo geolocation
+
+
+
+			}
+
+			function onError(error) {
+                <script>
+				alert('code: ' + error.code + '\n' +
+					'message: ' + error.message + '\n');
+                </script>
+			}
+		}
+?>
