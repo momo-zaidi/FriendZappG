f7190b1fc37de8f094b75f483c517e02408e3b4a
mickael111 <mickaellievens@hotmail.com>
2017-04-25 16:02:18 +0200

    Update GetInscription.php

--- GetInscription.php
+++ GetInscription.php
@@ -1,5 +1,6 @@
 <?php
-				if (isset($_POST['inscrire']))
+
+				if (isset($_POST['inscrire'])
 				{
 				
 				IF    (empty($_POST['nom']) or empty($_POST['prenom']) or empty($_POST['password']) or empty($_POST['mail']) or empty($_POST['age']))
