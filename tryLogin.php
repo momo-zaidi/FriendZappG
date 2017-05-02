100b289ab17e75208ed27f763c35dac212f8c571
abdelrahman12 <abdel-x6@hotmail.com>
2017-05-02 15:28:06 +0200

    Update tryLogin.php

--- tryLogin.php
+++ tryLogin.php
@@ -1,1 +1,12 @@
+<?php
 
+if (empty($_POST['login']) OR empty($_POST['password']))
+{
+	print "<div class='alert alert-danger'>
+  <strong>Introduisez le Login et password S.V.P</strong>
+</div>";
+	
+}	
+
+
+?>
