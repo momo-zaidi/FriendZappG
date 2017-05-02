a3ce24d72e7ebbbedc3fb2281671686eb125aaeb
abdelrahman12 <abdel-x6@hotmail.com>
2017-05-02 15:53:13 +0200

    Update tryLogin.php

--- tryLogin.php
+++ tryLogin.php
@@ -8,5 +8,26 @@ if (empty($_POST['login']) OR empty($_POST['password']))
 	
 }	
 
+if ($utilisateur = $stmt->fetch(PDO::FETCH_ASSOC))
+{
+	session_start();
+	
+	$_SESSION['name']=$_POST['login'];
+	$_SESSION['id_user']=$utilisateur['id'];
+	$_SESSION['role']=$utilisateur['role'];
+	
+	
+	header('Location: sport.php');
+}
+else
+{
+	if (!empty($_POST['login']) AND !empty($_POST['password']))
+{
+	print "<div class='alert alert-danger'>
+	<strong>Login et/ou Password incorrect !</strong>
+	</div>";
+}
+	
+}
 
 ?>
