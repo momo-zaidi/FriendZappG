605d1c65cd327c43c0795fbe21fcabf9875261f6
momo-zaidi <momo-zaidi@hotmail.fr>
2017-05-09 15:11:05 +0200

    Update function.php

--- function.php
+++ function.php
@@ -1,9 +1,28 @@
-function VerificationInscription($mail)
-{
-    return 0;
+<?php
+
+
+function verification_inscription($email){
+   //on va verifier si l'user existe dans la base de donnée si oui on revois un false pour dire qu il n a pas été inscris
+   $sql = mysql_query("SELECT * FROM User WHERE ".$email." = '".$_POST['email']."'") or die(mysql_error()); 
+ 
+   if(mysql_num_rows ($sql == 0)
+    {
+        return false;
+    }
+  
+    return false;
 }
 
-function add_user($prenom, $nom, $mail, $password, $age)
+function add_user($lastname,$firstname,$email,$password,$birthday)
 {
-
+ 
+  
+  $sql = $dbh->prepare("insert into Users values ('',:email,:password")) ;
+  $sql->bindParam(':email', $email,PDO::PARAM_STR);
+  $sql->bindParam(':password', $password,PDO::PARAM_STR);
+  $sql->execute();
+  
+   
 }
+
+?>
